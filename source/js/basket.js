609e50bb1a8ef8655c0b2351fae619e8d7a8cc05
Janez Troha <janez.troha@gmail.com>
2010-12-15 13:58:29 +0100

    Fix for continue outside loop Some google closure compiler errors in ejs

--- source/js/basket.js
+++ source/js/basket.js
@@ -25,18 +25,18 @@ function drop(e) {
     var files = e.dataTransfer.files;
     if (files.length > 5) {
     	alert("Select no more than 5 files!")
-        continue;
-    };
-    for (var i = 0; i < files.length; i++) {
-        var file = files[i];
-
-        if (!file.type.match(/application\/x-bittorrent/)) {
-            continue;
-        }
-        if (file.fileSize > 200000) {
-            alert("File too large! Are you sure it's torrent?")
-            continue;
-        };
-        addTorrent(file);
-    }
+    }else{
+	    for (var i = 0; i < files.length; i++) {
+	        var file = files[i];
+	
+	        if (!file.type.match(/application\/x-bittorrent/)) {
+	            continue;
+	        }
+	        if (file.fileSize > 200000) {
+	            alert("File too large! Are you sure it's torrent?")
+	            continue;
+	        };
+	        addTorrent(file);
+	    }  
+	}
 }
